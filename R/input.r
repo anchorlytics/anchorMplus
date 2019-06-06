f2a34741f70fc6d529d32cedbe66a4928867d512
Sean Ho <git@seanho.com>
2019-06-06 12:39:38 -0700

    tidy up wordwrap

--- R/input.r
+++ R/input.r
@@ -3,23 +3,25 @@
 #' Word wrap to accommodate Mplus line length limitations.
 #'
 #' Mplus input syntax is limited to 90 characters on a line.
-#' This function takes a character vector, concatenates it with spaces,
-#' and word-wraps the result, returning a single string.
+#' This function is a wrapper around base::strwrap with reasonable defaults.
 #'
-#' @param .list a character vector
+#' @param x a character vector
 #' @param width passed to [base::strwrap()]
 #' @param exdent passed to [base::strwrap()]
 #' @param ... passed to [base::strwrap()]
 #' @return a single string, potentially with newlines
 #'
 #' @export
-#' @family Mplus helpers
+#' @family Input syntax
 #' @author Sean Ho <anchor@seanho.com>
 #'
 #' @examples
 #' cat(wordwrap(rownames(mtcars)))
-wordwrap <- function(.list, width = 80, exdent = 4, ...) {
-  paste(
-    strwrap(paste(.list, collapse = " "), width = width, exdent = exdent, ...),
-    collapse = "\n")
+wordwrap <- function(x, width = 80, exdent = 4, ...) {
+  paste(strwrap(
+    paste(x, collapse = " "),
+    width = width,
+    exdent = exdent,
+    ...
+  ), collapse = "\n")
 }
